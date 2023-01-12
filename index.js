ca128b960481c96fa43e22dc018a1be476036f47
ryanjwong <ryanjwong007@gmail.com>
2023-01-11 17:29:03 -0800

    Added API functionality

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const Hyperswarm = require('hyperswarm')
 const prompt = require('prompt-sync')({sigint: true});
+const fetch = require('node-fetch')
 
-
+async function init(key) {
+    await fetch(`http://localhost:3000/api/swarm?key=${key}`, {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    })
+    .then(response => response.json())
+    .then(response => console.log(JSON.stringify(response)))
+}
 
 async function main() {
     var valid = false
     var key
     const type = "code"
-    const body = "print(\'Hello World\')"
+    const body = "print(\'Hello World! My name is testnet\')"
     const id = 12
 
     while (!valid) {
@@ -44,7 +55,7 @@ async function main() {
         }`)
     })
     
-
+    await init(key)
     const topic = Buffer.alloc(32).fill(key) // A topic must be 32 bytes
     const discovery = swarm.join(topic, { server: false, client: true })
     await discovery.flushed() // Waits for the topic to be fully announced on the DHT
